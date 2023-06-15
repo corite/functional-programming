e7a0430eaacfb71a46941c6628dc64f496dcaa10
corite <corite@tuta.io>
2023-06-15 09:45:40 +0200

    feat: assignment 8

--- assignment_7.ml
+++ assignment_7.ml
@@ -4,3 +4,17 @@ type expr =
   | App of expr * expr (* function application *)
   | Let of string * expr * expr (* variable binding *)
   | LetRec of string * expr * expr (* recursive variable binding *)
+
+let rec expr2string = function
+  | Var v -> v
+  | Lambda (v, e) -> "λ" ^ v ^ "." ^ expr2string e
+  | App (e1, e2) -> expr2string e1 ^ expr2string e2
+  | Let (n, e1, e2) -> n ^ expr2string e1 ^ expr2string e2
+  | LetRec (n, e1, e2) -> n ^ expr2string e1 ^ expr2string e2
+
+let rec subst e v s = match e with
+| Var v -> s
+| Lambda (v, e) -> Lambda (v, subst e v s)
+| App (e1, e2) -> App (subst e1 v s, subst e2 v s)
+| Let (n, e1, e2) -> Let (n, subst e1 v s, subst e2 v s)
+| LetRec (n, e1, e2) -> LetRec (n, subst e1 v s, subst e2 v s)
---
+++ assignment_8.ml
@@ -0,0 +1,41 @@
+type expr =
+  | Var of string (* variable *)
+  | Lambda of string * expr (* lambda v . e *)
+  | App of expr * expr (* ( e1 e2 ) *)
+  | Let of string * expr * expr (* let v = e1 in e2 *)
+
+let rec expr2string = function
+  | Var s -> s
+  | Lambda (s, e) -> "λ" ^ s ^ "." ^ expr2string e
+  | App (e1, e2) -> "(" ^ expr2string e1 ^ " " ^ expr2string e2 ^ ")"
+  | Let (s, e1, e2) ->
+      "let " ^ s ^ "=" ^ expr2string e1 ^ " in " ^ expr2string e2
+
+let freevars e =
+  let rec fv = function
+    | Var s, bv -> if List.exists (fun e -> e = s) bv then [] else [ s ]
+    | Lambda (s, e), bv -> fv (e, s :: bv)
+    | App (e1, e2), bv -> fv (e1, bv) @ fv (e2, bv)
+    | Let (s, e1, e2), bv -> fv (e1, bv) @ fv (e2, s :: bv)
+  in
+  (fv e, [])
+
+let rec subst = function
+  | Var s, x, y when s = x -> y
+  | Lambda (s, e), x, y when s != x -> Lambda (s, subst (e, x, y))
+  | App (e1, e2), x, y -> App (subst (e1, x, y), subst (e2, x, y))
+  | Let (s, e1, e2), x, y when s != x ->
+      Let (s, subst (e1, x, y), subst (e2, x, y))
+  | e, x, y -> e
+
+let rec beta = function
+  | App (Lambda (s, e), a) -> subst (e, s, a)
+  | App (e1, e2) -> App (beta e1, beta e2)
+  | Lambda (s, e) -> Lambda (s, beta e)
+  | e -> e
+
+let rec remove_let = function
+  | Let (s, e1, e2) -> App (Lambda (s, e2), e1)
+  | Lambda (s, e) -> Lambda (s, remove_let e)
+  | App (e1, e2) -> App (remove_let e1, remove_let e2)
+  | e -> e
